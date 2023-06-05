d759137e63c7900f019a6035289fe9d89b24e509
coopy <crat@stanford.edu>
2023-06-05 15:28:21 -0500

    better truncation example

--- examples/truncation_example.py
+++ examples/truncation_example.py
@@ -1,6 +1,11 @@
 import asyncio
+import gzip
+import os
+import random
+from itertools import islice
 from os import getenv
 
+import requests
 from dotenv import load_dotenv
 from pydantic import BaseModel
 
@@ -11,26 +16,46 @@ load_dotenv()
 API_KEY = getenv("OPENAI_API_KEY")
 
 
-class SummarySchema(BaseModel):
-    summary: str
+class SentimentSchema(BaseModel):
+    sentiment: str
 
 
 SYSTEM_PROMPT = """
-Write a three-sentence summary of the user's text input.
+Analyze the sentiment of the given text.
+
+Few-shot examples:
+{examples}
 
 Respond with the following JSON schema:
 
 {json_schema}
 """
 
-USER_PROMPT = """{long_text}"""
-
 
 async def runner():
-    with open("examples/raw_data/long_text.txt", "r") as f:
-        long_text = f.read()
+    print("Downloading few-shot data...")
+    from datasets import load_dataset
 
-    gpt_json = GPTJSON[SummarySchema](API_KEY)
+    dataset = load_dataset("amazon_reviews_multi", "en", streaming=True)
+    train_dataset = dataset["train"]
+    examples = [ex for ex in islice(train_dataset, 200)]
+    random.shuffle(examples)
+    print("Done!")
+
+    def render_example(ex) -> str:
+        return f"Text: {ex['review_body']}\nSentiment: {'positive' if ex['stars'] > 3 else 'negative'}"
+
+    def render_examples(examples) -> str:
+        return "\n".join(render_example(ex) for ex in examples)
+
+    # to truncate a single few-shot example, we define a function that
+    # removes the last 2 lines of the few-shot area text
+    def few_shot_truncate_next(
+        text: str,
+    ) -> str:
+        return "\n".join(text.split("\n")[:-2])
+
+    gpt_json = GPTJSON[SentimentSchema](API_KEY)
     response, _ = await gpt_json.run(
         messages=[
             GPTMessage(
@@ -39,20 +64,22 @@ async def runner():
             ),
             GPTMessage(
                 role=GPTMessageRole.USER,
-                content=USER_PROMPT,
+                content="Text: I love this product. It's the best thing ever!",
             ),
         ],
         format_variables={
-            "long_text": long_text,
+            "examples": render_examples(examples),
         },
+        # if you don't truncate, the prompt will be too long
         truncation_options=TruncationOptions(
-            target_variable="long_text",
-            truncation_mode=VariableTruncationMode.BEGINNING,
-            max_prompt_tokens=300,
+            target_variable="examples",
+            max_prompt_tokens=8000,  # close to max gpt4 can handle
+            truncation_mode=VariableTruncationMode.CUSTOM,
+            custom_truncate_next=few_shot_truncate_next,
         ),
     )
     print(response)
-    print(f"Summary: {response.summary}")
+    print(f"Detected sentiment: {response.sentiment}")
 
 
 asyncio.run(runner())
